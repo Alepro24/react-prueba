7a83b471e66354041f09043844791b4db2297b06
Alepro24 <alepro0224@gmail.com>
2021-09-17 21:39:52 -0400

    Commit 3 Estilos

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,23 +1,47 @@
 
 import React, {Component} from 'react';
+import './Task.css';
 
 class Task extends Component{
+    //Otra funcion  para cambiar estilos
+    StyleCompleted() {
+        return{
+            fontSize: '20px',
+            color: this.props.tarea.done ? 'gray' : 'black', //condicional if
+            textDecoration: this.props.tarea.done ? 'line-through' : 'none'
+        }
+    }
+    
+    
     render(){
         const {tarea} = this.props;
-        
+         // <p className="red"> </p>
         return(
-            <div>
+            <p style={this.StyleCompleted()}> 
                 {tarea.title} - 
                 {tarea.description} - 
                 {tarea.done} - 
                 {tarea.id}
             <input type="checkbox"></input>
-            <button>
+            <button style={btnDelete}>
                 X
             </button>
-            </div>
+            </p>
         )
     }
+}
+
+//Estilos en linea
+
+const btnDelete = {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15 px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+
 }
 
 export default Task;
